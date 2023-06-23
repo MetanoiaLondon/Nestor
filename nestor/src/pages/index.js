a2aea1bb5f7393b02147b37959dfd24d6a5f1c83
Alan Warr <65758146+MetanoiaLondon@users.noreply.github.com>
2023-06-23 16:56:38 +0000

    Basic UI

--- nestor/src/pages/index.js
+++ nestor/src/pages/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import ReactMarkdown from 'react-markdown'
 import Head from "next/head";
 import { createParser } from "eventsource-parser";
-const APIKEY = process.env.REACT_APP_OPENAI_API_KEY;
+//const APIKEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 export default function Home() {
   const [userMessage, setUserMessage] = useState("");
